9a6dfb436753ad4f46bf1dfc6a78e34bb923c1ed
Kinshuk <kinshuk.ram@gmail.com>
2017-12-29 23:50:37 +0100

    Add the constants header file

---
+++ src/constants.h
@@ -0,0 +1,9 @@
+//
+// Created by Kinshuk Chandra on 27.12.17.
+//
+
+#ifndef PATH_PLANNING_CONSTANTS_H
+#define PATH_PLANNING_CONSTANTS_H
+
+#define REF_VAL 49.5
+#endif //PATH_PLANNING_CONSTANTS_H
